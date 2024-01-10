13c651fee41bfbb79598bd6d7ebbabff65c15313
Shaggggy_ <79113250+2shaggggy@users.noreply.github.com>
2024-01-10 15:39:10 +0100

    Wprowadzenie funkcji getopt

--- main.c
+++ main.c
@@ -1,16 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <getopt.h>
 #include "mrufka.h"
 
 
 
-int main(){
+int main(int argc, char **argv){
+
+    int option;
+    int size_x;
+    int size_y;
+
+    if (argc < 2){
+
+
+    }
+
+    while ((option = getopt(argc, argv, "s:i:r:f:m:n:")) !=-1){
+        switch (option){
+            // random - procent przeskód
+            case 'r' :
+            // file - wczytywanie z pliku 
+            case 'f' :
+            // save - plik do wypisywania  
+            case 's' :
+            // iteracja 
+            case 'i' :
+            // m - wysokość 
+            case 'm' :
+                size_x = atoi(optarg);
+            // n - szerokość 
+            case 'n' :
+                size_y = atoi(optarg);
+            default : 
+
+        }
+    }
+
+    if (size_x == 0 || size_y == 0){
+        printf("Błąd! Zły typ wywoływanych argumentów!\n");
+        return 0;
+    }
+
+    simulation simulation = generate_grid(size_x, size_y);
     
-    simulation simulation = generate_grid(2, 2);
-    
-    simulation->current_pos.x = 1;
-    simulation->current_pos.y = 1;
+    simulation->current_pos.x = (size_x/2);
+    simulation->current_pos.y = (size_y/2);
     simulation->direction = 0;
 
     ant_loop (simulation);
